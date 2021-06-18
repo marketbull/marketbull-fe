d0e6d895539cad01830264dc4abbcac7178ecb96
Olayinka Okewale <okjool2012@gmail.com>
2021-06-18 05:47:09 +0200

    updated the assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useWallet } from 'react-binance-wallet';
 
 function App() {
+  const { account, connect, reset, status, error, balance, chainId } = useWallet()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MarketBull.Finance</h1>
+      {status === 'disconnected' ? (
+        <>
+        <button style={{ display: 'block', marginBottom: 16 }} onClick={() => connect('injected')}>Connect Metamask</button>
+        <button style={{ display: 'block' }} onClick={() => connect('bsc')}>Connect Binance Chain Wallet</button>
+        </>
+      ) : (
+        <button onClick={() => reset()}>Disconnect</button>
+      )}
+      { error?.message }
+      { chainId != null && <p>chainId: {chainId}</p> }
+      {account && <p>Connected as {account}</p>}
+      {Number(balance) >= 0 && <p>{balance}</p>}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { WalletProvider } from 'react-binance-wallet';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <WalletProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </WalletProvider>,
   document.getElementById('root')
 );
 
